99a83a8db5a41ffa5c86ab0e7d7430131285c8f7
Jacky <jacky0405@ymail.com>
2021-03-06 15:42:28 +0800

    React and API

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -15,16 +15,19 @@ interface Props {
     closeForm: () => void;
     createOrEdit: (activity: Activity) => void;
     deleteActivity: (id: string) => void;
+    submitting: boolean;
 }
 
 export default function ActivityDashboard({activities, selectActivity, deleteActivity,
-        selectedActivity, cancelSelectActivity, editMode, openForm, closeForm, createOrEdit}: Props) {
+        selectedActivity, cancelSelectActivity, editMode, openForm, 
+        submitting ,closeForm, createOrEdit}: Props) {
     return (
         <Grid>
             <Grid.Column width='10'>
                 <ActivityList activities={activities} 
-                selectActivity={selectActivity}
-                deleteActivity={deleteActivity}
+                    selectActivity={selectActivity}
+                    deleteActivity={deleteActivity}
+                    submitting={submitting}
                 />
             </Grid.Column>
             <Grid.Column width='6'>
@@ -35,7 +38,12 @@ export default function ActivityDashboard({activities, selectActivity, deleteAct
                     openForm={openForm}
                 />}
                 {editMode &&
-                <ActivityForm closeForm={closeForm} activity={selectedActivity} createOrEdit={createOrEdit}/>}
+                <ActivityForm 
+                    closeForm={closeForm} 
+                    activity={selectedActivity} 
+                    createOrEdit={createOrEdit}
+                    submitting={submitting}    
+                />}
             </Grid.Column>
         </Grid>
     )
